6abb7844c2a1989f4ba94bf264ef42f40347a544
juwaini <juwaini@gmail.com>
2015-03-14 11:03:17 +0800

    Adding a function to build 'q' in search string

--- slash/slash_util.py
+++ slash/slash_util.py
@@ -1,5 +1,5 @@
-
 def isSearch(text):
+    "To check the existence of keyword 'search' at the beginning of 'text'"
     words = text.split()
     if len(words) == 0:
         return False
@@ -9,6 +9,7 @@ def isSearch(text):
         return False
 
 def isCache(text):
+    "To check the existence of option '--no-cache' at the end of the 'text'"
     words = text.split()
     if len(words) == 0:
         return False
@@ -16,3 +17,26 @@ def isCache(text):
         return False
     else:
         return True
+
+def stringQuery(text):
+    "To build the parameter 'q' in search query"
+    return_string = ''
+    words = text.split()
+
+    if isSearch(text):
+        words = words[1:]
+
+    if isCache(text) == False:
+        words.pop()
+
+    if len(words) == 0:
+        return return_string
+    elif len(words) == 1:
+        return_string += words[0]
+        return return_string
+    else:
+        for x in range(0, len(words)-1):
+            return_string += words[x]
+            if x != len(words)-1:
+                return_string += '+'
+        return return_string
