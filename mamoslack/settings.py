f65fca984a43d20f3cbe43ee4fbd79aab99dd9e6
juwaini <juwaini@gmail.com>
2015-03-16 11:39:03 +0800

    Adding model to allow query to be saved in database

--- mamoslack/settings.py
+++ mamoslack/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'mamoslack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mamoslack',
+        'USER': 'root',
     }
 }
 
--- slash/models.py
+++ slash/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class SearchResult(models.Model):
+    q = models.CharField(max_length=20)
+    result = models.CharField(max_length=1000)
+    date = models.DateField()
